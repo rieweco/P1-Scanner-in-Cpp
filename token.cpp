905fe651ca086c021618f62a7007e77608fa9e58
Riewerts <carc35@delmar.umsl.edu>
2018-03-22 22:35:49 -0500

    added enums and finished class for scanner

--- token.cpp
+++ token.cpp
@@ -46,45 +46,53 @@ string enumToString(TokenID token)
 {
 	switch token
 	{
-		case identifier: return "identifier";
-		case keywordStart: return "keyword: Start";
-		case keywordStop: return "keyword: Stop";
-		case keywordIter: return "keyword: Iter";
-		case keywordVoid: return "keyword: Void";
-		case keywordVar: return "keyword: Var";
-		case keywordReturn: return "keyword: Return";
-		case keywordRead: return "keyword: Read";
-		case keywordPrint: return "keyword: Print";
-		case keywordProgram: return "keyword: Program";
-		case keywordIff: return "keyword: Iff";
-		case keywordThen: return "keyword: Then";
-		case keywordLet: return "keyword: Let";
-		case operatorEqual: return "operator: Equal";
-		case operatorLess: return "operator: Less Than";
-		case operatorGreater: return "operator: Greater Than";
-		case operatorColon: return "operator: Colon";
-		case operatorPlus: return "operator: Addition";
-		case operatorMinus: return "operator: Subtraction";
-		case operatorMultiply: return "operator: Multiplication";
-		case operatorDivide: return "operator: Division";
-		case operatorModulus: return "operator: Modulus";
-		case delimPeriod: return "delimiter: Period";
-		case delimLeftParenthesis: return "delimiter: Left Parenthesis";
-		case delimRightParenthesis: return "delimiter: Right Parenthesis";
-		case delimComma: return "delimiter: Comma";
-		case delimLeftBrace: return "delimiter: Left Brace";
-		case delimRightBrace: return "delimiter: Right Brace";
-		case delimSemicolon: return "delimiter: Semi-Colon";
-		case delimLeftBracket: return "delimiter: Left Bracket";
-		case delimRightBracket: return "delimiter: Right Bracket";
-		case integer: return "integer";
-		case comment: return "comment";
-		case endOfFile: return "end of file";
+		case identifier: return "IDtk";
+		case keywordStart: return "keywordtk: Start";
+		case keywordStop: return "keywordtk: Stop";
+		case keywordIter: return "keywordtk: Iter";
+		case keywordVoid: return "keywordtk: Void";
+		case keywordVar: return "keywordtk: Var";
+		case keywordReturn: return "keywordtk: Return";
+		case keywordRead: return "keywordtk: Read";
+		case keywordPrint: return "keywordtk: Print";
+		case keywordProgram: return "keywordtk: Program";
+		case keywordIff: return "keywordtk: Iff";
+		case keywordThen: return "keywordtk: Then";
+		case keywordLet: return "keywordtk: Let";
+		case operatorEqual: return "operatortk: Equal";
+		case operatorLess: return "operatortk: Less Than";
+		case operatorGreater: return "operatortk: Greater Than";
+		case operatorColon: return "operatortk: Colon";
+		case operatorPlus: return "operatortk: Addition";
+		case operatorMinus: return "operatortk: Subtraction";
+		case operatorMultiply: return "operatortk: Multiplication";
+		case operatorDivide: return "operatortk: Division";
+		case operatorModulus: return "operatortk: Modulus";
+		case delimPeriod: return "delimitertk: Period";
+		case delimLeftParenthesis: return "delimitertk: Left Parenthesis";
+		case delimRightParenthesis: return "delimitertk: Right Parenthesis";
+		case delimComma: return "delimitertk: Comma";
+		case delimLeftBrace: return "delimitertk: Left Brace";
+		case delimRightBrace: return "delimitertk: Right Brace";
+		case delimSemicolon: return "delimitertk: Semi-Colon";
+		case delimLeftBracket: return "delimitertk: Left Bracket";
+		case delimRightBracket: return "delimitertk: Right Bracket";
+		case integer: return "integertk";
+		case comment: return "commenttk";
+		case endOfFile: return "EOFtk";
 		default: return "****ERROR IN ENUMTOSTRING() FUNCTION! TOKENID NOT FOUND! ****";		
 	}
 }
 
-
+int enumToInt(TokenID token)
+{
+	switch token
+	{
+		case identifier: return 1000;
+		case integer: return 1001;
+		case endOfFile: return 
+	}
+}
 
 
 
