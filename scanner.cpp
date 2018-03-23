5e8fcafeb6edf335a8e00fc3fa392283498f6bb3
Riewerts <carc35@delmar.umsl.edu>
2018-03-22 23:45:17 -0500

    added switch statements for the incoming character read from the file

--- scanner.cpp
+++ scanner.cpp
@@ -10,21 +10,192 @@ using namespace std;
 
 
 
-string table_row_values = {"BOF","identifier", "integer", "<",">",":","+","-","*","/","%",".","(",")",",","{","}",";","[","]",
-			 "EOF"};
+string table_row_values = {"BOF","identifier", "integer", "<",">","=",":","+","-","*","/","%",".","(",")",",","{","}",";",
+			"[","]","EOF", "error"};
 
 int table_rows = {0,1000,1001,1002,1003,1004,1005,1006,1007,1008,1009,1010,1011,1012,1013,1014,1015,1016,1017,1018,
-			1019};
+			1019, 1020, -1};
 
-int FSA_TABLE[3][20] = {
+int FSA_TABLE[3][23] = {
 			{0,1,2,1002,1003,1004,1005,1006,1007,1008,1009,1010,1011,1012,1013,1014,1015,1016,1017,1018,
-			1019},
+			1019,1020,-1},
 			{1000,1,1,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,
-			1000},
+			1000,1000,-1},
 			{1001,1001,2,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,1001,
-			1001}
+			1001,1001,-1}
 			};
 
+int findTableRow(char next)
+{
+	switch next
+	{
+		case 'a': return 1;
+			break;
+		case 'A': return 1;
+			break;
+		case 'b': return 1;
+			break;
+		case 'B': return 1;
+			break;
+		case 'c': return 1;
+			break;
+		case 'C': return 1;
+			break;
+		case 'd': return 1;
+			break;
+		case 'D': return 1;
+			break;
+		case 'e': return 1;
+			break;
+		case 'E': return 1;
+			break;
+		case 'f': return 1;
+			break;
+		case 'F': return 1;
+			break;
+		case 'g': return 1;
+			break;
+		case 'G': return 1;
+			break;
+		case 'h': return 1;
+			break;
+		case 'H': return 1;
+			break;
+		case 'i': return 1;
+			break;
+		case 'I': return 1;
+			break;
+		case 'j': return 1;
+			break;
+		case 'J': return 1;
+			break;
+		case 'k': return 1;
+			break;
+		case 'K': return 1;
+			break;
+		case 'l': return 1;
+			break;
+		case 'L': return 1;
+			break;
+		case 'm': return 1;
+			break;
+		case 'M': return 1;
+			break;
+		case 'n': return 1;
+			break;
+		case 'N': return 1;
+			break;
+		case 'o': return 1;
+			break;
+		case 'O': return 1;
+			break;
+		case 'p': return 1;
+			break;
+		case 'P': return 1;
+			break;
+		case 'q': return 1;
+			break;
+		case 'Q': return 1;
+			break;
+		case 'r': return 1;
+			break;
+		case 'R': return 1;
+			break;
+		case 's': return 1;
+			break;
+		case 'S': return 1;
+			break;
+		case 't': return 1;
+			break;
+		case 'T': return 1;
+			break;
+		case 'u': return 1;
+			break;
+		case 'U': return 1;
+			break;
+		case 'v': return 1;
+			break;
+		case 'V': return 1;
+			break;
+		case 'w': return 1;
+			break;
+		case 'W': return 1;
+			break;
+		case 'x': return 1;
+			break;
+		case 'X': return 1;
+			break;
+		case 'y': return 1;
+			break;
+		case 'Y': return 1;
+			break;
+		case 'z': return 1;
+			break;
+		case 'Z': return 1;
+			break;
+		case '1': return 2;
+			break;
+		case '2': return 2;
+			break;
+		case '3': return 2;
+			break;
+		case '4': return 2;
+			break;
+		case '5': return 2;
+			break;
+		case '6': return 2;
+			break;
+		case '7': return 2;
+			break;
+		case '8': return 2;
+			break;
+		case '9': return 2;
+			break;
+		case '0': return 2;
+			break;
+		case '<': return 3;
+			break;
+		case '>': return 4;
+			break;
+		case '=': return 5;
+			break;
+		case ';': return 6;
+			break;
+		case '+': return 7;
+			break;
+		case '-': return 8;
+			break;
+		case '*': return 9;
+			break;
+		case '/': return 10;
+			break;
+		case '%': return 11;
+			break;
+		case '.': return 12;
+			break;
+		case '(': return 13;
+			break;
+		case ')': return 14;
+			break;
+		case ',': return 15;
+			break;
+		case '{': return 16;
+			break;
+		case '}': return 17;
+			break;
+		case ';': return 18;
+			break;
+		case '[': return 19;
+			break;
+		case ']': return 20;
+			break;
+		default: return -1;
+	}
+}
+
+
+
+//scanner class
 
 Scanner::Scanner() = default;
 Scanner::~Scanner() = default;
@@ -32,7 +203,29 @@ Scanner::~Scanner() = default;
 Token *Scanner::getToken(string &file, int index, int lineNumber)
 {
 	int currentState = 0;
-	 
+	string word;
+	int column = 0;
+	
+	//set newToken to endOfFile token before getting next char in case of EOF
+	TokenID newToken = endOfFile;
+	
+	//set flag for reading multiple chars
+	bool isToken = true;
+	
+	while(isToken)
+	{
+		char next = file[index];
+		
+		if(next)
+		{
+			currentState = FSA_TABLE[column][
+		}
+		else
+		{
+		//eof has been reached	
+		currentState = 1019;
+		}
+	}
 } 
 
 
--- token.cpp
+++ token.cpp
@@ -47,52 +47,77 @@ string enumToString(TokenID token)
 	switch token
 	{
 		case identifier: return "IDtk";
+				break;
 		case keywordStart: return "keywordtk: Start";
+				break;
 		case keywordStop: return "keywordtk: Stop";
+				break;
 		case keywordIter: return "keywordtk: Iter";
+				break;
 		case keywordVoid: return "keywordtk: Void";
+				break;
 		case keywordVar: return "keywordtk: Var";
+				break;
 		case keywordReturn: return "keywordtk: Return";
+				break;
 		case keywordRead: return "keywordtk: Read";
+				break;
 		case keywordPrint: return "keywordtk: Print";
+				break;
 		case keywordProgram: return "keywordtk: Program";
+				break;
 		case keywordIff: return "keywordtk: Iff";
+				break;
 		case keywordThen: return "keywordtk: Then";
+				break;
 		case keywordLet: return "keywordtk: Let";
+				break;
 		case operatorEqual: return "operatortk: Equal";
+				break;
 		case operatorLess: return "operatortk: Less Than";
+				break;
 		case operatorGreater: return "operatortk: Greater Than";
+				break;
 		case operatorColon: return "operatortk: Colon";
+				break;
 		case operatorPlus: return "operatortk: Addition";
+				break;
 		case operatorMinus: return "operatortk: Subtraction";
+				break;
 		case operatorMultiply: return "operatortk: Multiplication";
+				break;
 		case operatorDivide: return "operatortk: Division";
+				break;
 		case operatorModulus: return "operatortk: Modulus";
+				break;
 		case delimPeriod: return "delimitertk: Period";
+				break;
 		case delimLeftParenthesis: return "delimitertk: Left Parenthesis";
+				break;
 		case delimRightParenthesis: return "delimitertk: Right Parenthesis";
+				break;
 		case delimComma: return "delimitertk: Comma";
+				break;
 		case delimLeftBrace: return "delimitertk: Left Brace";
+				break;
 		case delimRightBrace: return "delimitertk: Right Brace";
+				break;
 		case delimSemicolon: return "delimitertk: Semi-Colon";
+				break;
 		case delimLeftBracket: return "delimitertk: Left Bracket";
+				break;
 		case delimRightBracket: return "delimitertk: Right Bracket";
+				break;
 		case integer: return "integertk";
+				break;
 		case comment: return "commenttk";
+				break;
 		case endOfFile: return "EOFtk";
+				break;
 		default: return "****ERROR IN ENUMTOSTRING() FUNCTION! TOKENID NOT FOUND! ****";		
 	}
 }
 
-int enumToInt(TokenID token)
-{
-	switch token
-	{
-		case identifier: return 1000;
-		case integer: return 1001;
-		case endOfFile: return 
-	}
-}
 
 
 
